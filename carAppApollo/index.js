ccec5e19ecc86df641d828388f5e82fa51a73391
Prem R <prem.rajah@gmail.com>
2020-11-27 18:34:14 +0000

    create apollo-server

--- carAppApollo/index.js
+++ carAppApollo/index.js
@@ -1,6 +1,5 @@
-const { graphql, buildSchema } = require('graphql')
-const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
+
+const { ApolloServer, gql } = require('apollo-server')
 
 // create a memory db
 const db = {
@@ -13,7 +12,7 @@ const db = {
 }
 
 // create schema
-const schema = buildSchema(`
+const schema = gql(`
     enum CarTypes {
         Sedan
         SUV
@@ -39,108 +38,42 @@ const schema = buildSchema(`
     }
 `)
 
-// create resolvers
-const resolvers = () => {
-    const carsByType = (args) => {
-        return db.cars.filter(car => car.type === args.type)
+// create resolver map
+const resolvers = {
+    Query: {
+        carsByType: (parent, args, context, info) => {
+            return db.cars.filter(car => car.type === args.type)
+        },
+        carsById: (parent, args, context, info) => {
+            return db.cars.filter(car => car.id === args.id)[0]
+        }
+    },
+    Car: {
+        brand: (parent, args, context, info) => {
+            return db.cars.filter(car => car.brand === parent.brand)[0].brand
+        }
+    },
+    Mutation: {
+        insertCar: (_, { brand, color, doors, type }) => {
+            db.cars.push({
+                id: Math.random().toString(),
+                brand: brand,
+                color: color,
+                doors: doors,
+                type: type
+            })
+            return db.cars
+        }
     }
-
-    const carsById = (args) => {
-        return db.cars.filter(car => car.id === args.id)[0]
-    }
-
-    const insertCar = ({ brand, color, doors, type }) => {
-        db.cars.push({
-            id: Math.random().toString(),
-            brand: brand,
-            color: color,
-            doors: doors,
-            type: type,
-        })
-        return db.cars
-    }
-
-
-    return { carsByType, carsById, insertCar }
 }
 
+// create apollo server
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers
+})
 
-// excute queries
-// const excuteQuery = async () => {
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`)
+})
 
-//     const queryByType = `
-//     {
-//         carsByType(type: Coupe) {
-//             brand
-//             color
-//             type
-//             id
-//         }
-//     }
-//     `
-
-//     const responseOne = await graphql(schema, queryByType, resolvers())
-//     // console.log(responseOne.data)
-
-//     const queryById = `
-//     {
-//         carsById(id: "b") {
-//         brand
-//         color
-//         type
-//         id
-//         }
-//     }
-//     `
-//     const responseTwo = await graphql(schema, queryById, resolvers())
-//     // console.log(responseTwo.data)
-
-//     const mutationQuery = `
-//         mutation {
-//             insertCar(brand: "Nissan", color: "Pink", doors: 5, type: SUV) {
-//                 brand
-//                 color
-//                 doors
-//                 id
-//             }
-//         }
-//     `
-
-//     const mutationResponse = await graphql(schema, mutationQuery, resolvers());
-//     // console.log(mutationResponse.data);
-
-//     const mutationsWithVariables = `
-//         mutation insertCar($brand: String!, $color: String!, $doors: Int!, $type: CarTypes!) {
-//             insertCar(brand: $brand, color: $color, doors: $doors, type: $type) {
-//                 brand
-//                 color
-//                 id
-//                 doors
-//             }
-//         }
-//     `
-
-//     const mutationVarResponse = await graphql(schema, mutationsWithVariables, resolvers(), null, {
-//         brand: 'Mustang',
-//         color: 'Orange',
-//         doors: 2,
-//         type: 'Coupe'
-//     })
-
-//     console.log(mutationVarResponse.data)
-
-
-// }
-// excuteQuery();
-
-
-
-// http server
-const app = express();
-
-app.use(
-    '/graphql',
-    graphqlHTTP({ schema: schema, rootValue: resolvers(), graphiql: true }))
-
-app.listen(3000)
-console.log('GraphQL server is listening to PORT 3000')
